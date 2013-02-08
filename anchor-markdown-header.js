8fed3b95cbb91545553ded4929c7bdb9566333f0
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-08 08:44:54 -0500

    including anchor generator code from node/tools/doc/html.js and passing all nodejs anchor tests

--- anchor-markdown-header.js
+++ anchor-markdown-header.js
@@ -1,6 +1,15 @@
 'use strict';
 
-module.exports = function anchorMarkdownHeader(header, mode) {
+// https://github.com/joyent/node/blob/192192a09e2d2e0d6bdd0934f602d2dbbf10ed06/tools/doc/html.js#L172-L183 
+function getNodejsId(text) {
+  text = text.toLowerCase();
+  text = text.replace(/[^a-z0-9]+/g, '_');
+  text = text.replace(/^_+|_+$/, '');
+  text = text.replace(/^([^a-z])/, '_$1');
+  return text;
+}
+
+module.exports = function anchorMarkdownHeader(header, mode, moduleName) {
   mode = mode || 'github';
   var replace;
 
@@ -12,9 +21,9 @@ module.exports = function anchorMarkdownHeader(header, mode) {
         };
       break;
     case 'nodejs.org': 
+      if (!moduleName) throw new Error('Need module name to generate proper anchor for ' + mode);
       replace = function (hd) {
-          return hd.replace(/[\( .]/g, '_')
-                   .replace(/[\)\[\],;]/g, '');
+          return getNodejsId(moduleName + '.' + hd);
       };
       break;
   }
--- test/anchor-markdown-header.js
+++ test/anchor-markdown-header.js
@@ -5,19 +5,42 @@ var test   =  require('trap').test
   , format =  require('util').format
   , anchor =  require('..')
 
-
-
+function checkResult(t, mode, moduleName, header, href) {
+  var expectedAnchor = format('[%s](%s)', header, href)
+  t.equal(anchor(header, mode, moduleName), expectedAnchor, format('generates "%s" for header "%s"', expectedAnchor, header));
+}
+  
 test('generating anchor in github mode', function (t) {
 
-  function check(header, href) {
-    var expectedAnchor = format('[%s](%s)', header, href)
-    t.equal(anchor(header), expectedAnchor, format('generates "%s" for header "%s"', expectedAnchor, header));
-  }
-  
+  var check = checkResult.bind(null, t, 'github', undefined);
+
   [ [ 'intro',  '#intro' ]
   , [ 'mineflayer.windows.Window (base class)',  '#mineflayerwindowswindow-base-class']
   , [ 'window.findInventoryItem(itemType, metadata, [notFull])', '#windowfindinventoryitemitemtype-metadata-notfull' ]
   , [ 'furnace "updateSlot" (oldItem, newItem)', '#furnace-updateslot-olditem-newitem' ]
+  , [ '"playerJoined" (player)', '#playerjoined-player' ]
   ].forEach(function (x) { check(x[0], x[1]) });
   
-});
+})
+
+test('generating anchor in nodejs.org mode for fs module', function (t) {
+
+  var check = checkResult.bind(null, t, 'nodejs.org', 'fs');
+
+  [ [ 'fs.rename(oldPath, newPath, [callback])', '#fs_fs_rename_oldpath_newpath_callback' ] 
+  , [ 'fs.truncate(fd, len, [callback])', '#fs_fs_truncate_fd_len_callback' ]
+  , [ 'fs.symlink(srcpath, dstpath, [type], [callback])', '#fs_fs_symlink_srcpath_dstpath_type_callback' ]
+  , [ "fs.appendFile(filename, data, encoding='utf8', [callback])", '#fs_fs_appendfile_filename_data_encoding_utf8_callback' ]
+  , [ 'Class: fs.FSWatcher', '#fs_class_fs_fswatcher' ]
+  ].forEach(function (x) { check(x[0], x[1]) });
+  
+})
+
+test('generating anchor in nodejs.org mode for crypto module', function (t) {
+
+  var check = checkResult.bind(null, t, 'nodejs.org', 'crypto');
+
+  [ [ 'cipher.update(data, [input_encoding], [output_encoding])', '#crypto_cipher_update_data_input_encoding_output_encoding' ]
+  , [ 'crypto.pbkdf2(password, salt, iterations, keylen, callback)' , '#crypto_crypto_pbkdf2_password_salt_iterations_keylen_callback' ]
+  ].forEach(function (x) { check(x[0], x[1]) });
+})
