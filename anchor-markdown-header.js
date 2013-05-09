f8befce28de7991eec1b4564817a5497be6f38ad
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-09 17:56:57 -0400

    improved docs

--- anchor-markdown-header.js
+++ anchor-markdown-header.js
@@ -49,8 +49,8 @@ function getBitbucketId(text, repetition) {
  * @function
  * @param header      {String} The header to be anchored.
  * @param mode        {String} The anchor mode ('github.com'|'nodejs.org|bitbucket.org').
- * @param moduleName  {String} The name of the module of the given header (required only for 'nodejs.org' mode).
  * @param repetition  {Number} The nth occurrence of this header text, starting with 0. Not required for the 0th instance.
+ * @param moduleName  {String} The name of the module of the given header (required only for 'nodejs.org' mode).
  * @return            {String} The header anchor that is compatible with the given mode.
  */
 module.exports = function anchorMarkdownHeader(header, mode, repetition, moduleName) {
