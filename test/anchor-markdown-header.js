7c33d3e729e53cb61be0fb217676408743dd0f7f
Florian Keller <keller@toechtersoehne.com>
2016-04-12 18:58:50 +0200

    Add mdash and ndash

--- test/anchor-markdown-header.js
+++ test/anchor-markdown-header.js
@@ -39,6 +39,7 @@ test('\ngenerating anchor in github mode', function (t) {
   , [ 'class~method', null, '#classmethod']
   , [ 'func($event)', null, '#funcevent']
   , [ 'trailing *', null, '#trailing-']
+  , [ 'replace – or —', null, '#replace--or-']
   ].forEach(function (x) { check(x[0], x[1], x[2]) });
   t.end();
 })
